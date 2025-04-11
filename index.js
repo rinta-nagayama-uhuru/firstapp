f390e688e2f9b7deffd83060baacae58b0ba78a6
P675\P675-2593 <nagayama.rinta@uhuru.jp>
2025-04-11 11:55:35 +0900

    投稿一覧表示のコントローラー作成

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+const routers = require("./routes");
+app.use(routers);
+
 //localhost:3000で起動
 app.listen(3000, () =>{
   console.log("Listening on localhost port 3000");
