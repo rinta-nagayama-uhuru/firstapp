0962a87e3856bad33d8246a81f80a09bea7508bc
P675\P675-2593 <nagayama.rinta@uhuru.jp>
2025-04-28 12:23:02 +0900

    正常に投稿できるテスト

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,22 +1,47 @@
 const request = require('supertest');
 const app = require('../app');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-describe('ページ遷移のテスト', () => {
-  describe('トップページ遷移のテスト', () => {
-    it('トップページに正しく遷移できる', async () => {
-       const response = await request(app)
-         .get("/posts")
-         .expect(200);
-       expect(response.text).toMatch(/トップページ/);
-    }, 100000);
-  });
+describe('Webアプリケーションのテスト', () => {
+  describe('投稿のテスト', () => {
+    afterEach(async () => {
+      await prisma.post.deleteMany({});
+      //各テストの後にデータベースを削除する
+    })
 
-  describe('トップページの遷移に失敗', () => {
-    it('未定義のパスにアクセスしたとき、リダイレクトして302エラーを返す', async () => {
-      const response = await request(app)
-        .get('/some-undefined-path')
-        .expect(302);
-      expect(response.headers.location).toEqual('/non-existent-page');
+    // 正常系
+    it('新しい投稿を保存できる', async () => {
+      const newPost = await prisma.post.create({
+        data: {
+          content: 'これはテスト投稿です。',
+        },
+      });
+
+      expect(newPost).not.toBeNull();
+      expect(newPost.content).toBe('これはテスト投稿です。');
     });
   });
-});
+
+  // 正常系
+  describe('ページ遷移のテスト', () => {
+    describe('トップページ遷移のテスト', () => {
+      it('トップページに正しく遷移できる', async () => {
+         const response = await request(app)
+           .get("/posts")
+           .expect(200);
+         expect(response.text).toMatch(/トップページ/);
+      }, 100000);
+    });
+  
+    // 異常系
+    describe('トップページの遷移に失敗', () => {
+      it('未定義のパスにアクセスしたとき、リダイレクトして302エラーを返す', async () => {
+        const response = await request(app)
+          .get('/some-undefined-path')
+          .expect(302);
+        expect(response.headers.location).toEqual('/non-existent-page');
+      });
+    });
+  });
+})
