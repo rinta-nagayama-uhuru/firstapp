be6a583919bf6ec3678ade9c0e9214171eb16238
P675\P675-2593 <nagayama.rinta@uhuru.jp>
2025-04-28 14:03:52 +0900

    正常に投稿できないテスト

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,11 +4,40 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 describe('Webアプリケーションのテスト', () => {
+
+  describe('ページ遷移のテスト', () => {
+    // 正常系
+    describe('トップページ遷移のテスト', () => {
+      it('トップページに正しく遷移できる', async () => {
+         const response = await request(app)
+           .get('/posts')
+           .expect(200);
+         expect(response.text).toMatch(/トップページ/);
+      });
+
+      it('新規投稿画面に正しく遷移できる', async () => {
+        const response = await request(app)
+          .get('/posts/create')
+          .expect(200);
+        expect(response.text).toMatch(/新規投稿ページ/);
+     });
+    });
+  
+    // 異常系
+    describe('トップページの遷移に失敗', () => {
+      it('未定義のパスにアクセスした時、リダイレクトして302エラーを返す', async () => {
+        const response = await request(app)
+          .get('/some-undefined-path')
+          .expect(302);
+        expect(response.headers.location).toEqual('/non-existent-page');
+      });
+    });
+  });
+
   describe('投稿のテスト', () => {
     afterEach(async () => {
       await prisma.post.deleteMany({});
-      //各テストの後にデータベースを削除する
-    })
+    });
 
     // 正常系
     it('新しい投稿を保存できる', async () => {
@@ -21,27 +50,15 @@ describe('Webアプリケーションのテスト', () => {
       expect(newPost).not.toBeNull();
       expect(newPost.content).toBe('これはテスト投稿です。');
     });
-  });
 
-  // 正常系
-  describe('ページ遷移のテスト', () => {
-    describe('トップページ遷移のテスト', () => {
-      it('トップページに正しく遷移できる', async () => {
-         const response = await request(app)
-           .get("/posts")
-           .expect(200);
-         expect(response.text).toMatch(/トップページ/);
-      }, 100000);
-    });
-  
     // 異常系
-    describe('トップページの遷移に失敗', () => {
-      it('未定義のパスにアクセスしたとき、リダイレクトして302エラーを返す', async () => {
-        const response = await request(app)
-          .get('/some-undefined-path')
-          .expect(302);
-        expect(response.headers.location).toEqual('/non-existent-page');
-      });
+    it('空のcontentで投稿を作成しようとするとエラーが発生する', async () => {
+      const response = await request(app)
+        .post('/posts/create')
+        .send({ content: '' })
+        .expect('Content-Type', /json/)
+        .expect(400);
+      expect(response.body.message).toEqual('Content cannot be empty');
     });
   });
-})
+});
